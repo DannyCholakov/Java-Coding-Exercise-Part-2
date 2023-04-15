8ea4fd84eb42a8020b7d0d672c70207c13e7c408
Daniel Cholakov <danielcholakov133@gmail.com>
2023-04-15 21:19:26 +0300

    Update

--- src/NumberOfDaysInMonth.java
+++ src/NumberOfDaysInMonth.java
@@ -31,15 +31,13 @@ public class NumberOfDaysInMonth {
         if (month < 1 || month > 12 || year < 1 || year > 9999)
             return -1;
         switch (month) {
-            case 4: case 6: case 9: case 11:
-                return 30;
-            case 2:
+            case 4, 6, 9, 11 -> {return 30;}
+            case 2 -> {
                 if (isLeapYear(year))
                     return 29;
                 return 28;
-            default:
-                return 31;
+            }
+            default -> {return 31;}
         }
-
     }
 }
